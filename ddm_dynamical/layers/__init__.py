e72151ff2d7a0adfb31ab080c576518236b975b6
tobifinn <tobias.finn@enpc.fr>
2023-08-27 19:22:02 +0200

    Fixed bug of missing polynomial layer

--- ddm_dynamical/layers/__init__.py
+++ ddm_dynamical/layers/__init__.py
@@ -1,12 +1,10 @@
 from .film import FilmLayer
-from .polynomial import PolynomialLayer
 from .random_fourier import RandomFourierLayer
 from .time_embedding import SinusoidalTimeEmbedding
 
 
 __all__ = [
     "FilmLayer",
-    "PolynomialLayer",
     "RandomFourierLayer",
     "SinusoidalTimeEmbedding"
 ]
