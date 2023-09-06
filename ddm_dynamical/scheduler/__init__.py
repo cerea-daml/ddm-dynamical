eeb79b4685f9e04b5bff11a52cc06beef6749068
tobifinn <tobias.finn@enpc.fr>
2023-09-06 15:53:21 +0200

    Added more scheduler

--- ddm_dynamical/scheduler/__init__.py
+++ ddm_dynamical/scheduler/__init__.py
@@ -1,6 +1,10 @@
 from .ddpm_scheduler import DDPMScheduler
+from .linear_scheduler import LinearScheduler
+from .nn_scheduler import NNScheduler
 
 
 __all__ = [
     "DDPMScheduler",
+    "LinearScheduler",
+    "NNScheduler"
 ]
