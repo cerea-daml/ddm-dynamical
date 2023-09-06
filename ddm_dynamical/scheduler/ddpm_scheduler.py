34aa5c6f199b22be040340e3655b15324ba76c42
tobifinn <tobias.finn@enpc.fr>
2023-09-06 15:35:40 +0200

    Renamed linear scheduler into ddpm scheduler

---
+++ ddm_dynamical/scheduler/ddpm_scheduler.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+# Created for ddm_dynamical
+#
+# @author: Tobias Sebastian Finn, tobias.finn@enpc.fr
+#
+#    Copyright (C) {2023}  {Tobias Sebastian Finn}
+
+
+# System modules
+import logging
+
+# External modules
+import torch
+
+# Internal modules
+from .noise_scheduler import NoiseScheduler
+
+
+logger = logging.getLogger(__name__)
+
+
+class DDPMScheduler(NoiseScheduler):
+    def __init__(self):
+        """
+        Noise scheduling as proposed by Ho et al. 2020 and
+        approximated in Kingma et al. 2021.
+        """
+        super().__init__()
+        self.shift = 1E-4
+        self.scale = 10.
+
+    def get_gamma(self, timestep: torch.Tensor) -> torch.Tensor:
+        return -torch.log(
+            torch.expm1(self.shift + self.scale * timestep.pow(2))
+        )
