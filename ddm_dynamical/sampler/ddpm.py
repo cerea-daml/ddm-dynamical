4fb567b2243475b34f39dcd606edc7d33d468392
tobifinn <tobias.finn@enpc.fr>
2023-09-06 17:34:00 +0200

    Adapted DDPM scheduler

--- ddm_dynamical/sampler/ddpm.py
+++ ddm_dynamical/sampler/ddpm.py
@@ -23,34 +23,30 @@ main_logger = logging.getLogger(__name__)
 class DDPMSampler(BaseSampler):
     def forward(
             self,
-            in_data: torch.Tensor,
-            idx_time: int = 1000
+            in_tensor: torch.Tensor,
+            step: torch.Tensor
     ) -> torch.Tensor:
-        curr_idx_time = torch.round(idx_time * self.step_mul).long()
-        prev_idx_time = torch.round((idx_time - 1) * self.step_mul).long()
-        alpha_t = self.noise_scheduler.get_alpha(curr_idx_time)
-        alpha_s = self.noise_scheduler.get_alpha(prev_idx_time)
+        # Estimate coefficients from scheduler
+        gamma_t = self.scheduler.get_gamma(step)
+        gamma_s = self.scheduler.get_gamma(step-1/self.timesteps)
+        var_t = torch.sigmoid(gamma_t)
+        alpha_t = (1-var_t)
+        alpha_s = torch.sigmoid(-gamma_s)
         alpha_dash_t = alpha_t / alpha_s
-        alpha_sqrt_t = alpha_t.sqrt()
         alpha_sqrt_s = alpha_s.sqrt()
         alpha_dash_sqrt_t = alpha_dash_t.sqrt()
-        sigma_t = self.noise_scheduler.get_sigma(curr_idx_time)
 
+        # Estimate factors
         latent_factor = alpha_dash_sqrt_t*(1-alpha_s)/(1-alpha_t)
         state_factor = alpha_sqrt_s*(1-alpha_dash_t)/(1-alpha_t)
         noise_factor = ((1-alpha_dash_t)*(1-alpha_s)/(1-alpha_t)).sqrt()
 
-        time_tensor = torch.full(
-            torch.Size(
-                [in_data.shape[0]] + [1, ] * (in_data.ndim - 1)
-            ), curr_idx_time, device=in_data.device,
-            dtype=in_data.dtype, layout=in_data.layout
-        )
-        prediction = self.denoising_model(in_data, time_tensor)
-        noise = torch.randn_like(in_data)
+        # Estimate tensors
+        time_tensor = torch.ones_like(in_tensor) * step
+        prediction = self.denoising_model(in_tensor, time_tensor)
+        state = (in_tensor-var_t.sqrt()*prediction) / (1-var_t).sqrt()
+        noise = torch.randn_like(in_tensor)
 
-        return latent_factor * in_data + \
-            state_factor * self.head.get_state(
-                in_data, prediction=prediction,
-                alpha_sqrt=alpha_sqrt_t, sigma=sigma_t
-            ) + noise_factor * noise
+        out_state = latent_factor * in_tensor + state_factor * state + \
+                    noise_factor * noise
+        return out_state
