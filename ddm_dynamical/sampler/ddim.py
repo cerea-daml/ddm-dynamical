d0bddff5043b88066fd5a51bc69f4d1adc7c3e26
tobifinn <tobias.finn@enpc.fr>
2023-10-06 19:24:13 +0200

    Cleaned samplers

--- ddm_dynamical/sampler/ddim.py
+++ ddm_dynamical/sampler/ddim.py
@@ -40,17 +40,20 @@ class DDIMSampler(BaseSampler):
         self.ddpm = ddpm
         self.eta = eta
 
-    def _estimate_stoch_level(self, alpha_t, alpha_s):
+    def _estimate_stoch_level(self, alpha_t_sq, alpha_s_sq):
         if self.ddpm:
-            sigma_t_2 = (1 - alpha_s + 1E-9) / (1 - alpha_t + 1E-9) \
-                        * (1 - (alpha_t + 1E-9) / (alpha_s + 1E-9))
-            det_level = (1 - alpha_s - sigma_t_2).sqrt()
-            stoch_level = torch.sqrt(1 - (alpha_t + 1E-9) / (alpha_s + 1E-9))
+            sigma_t_2 = (1 - alpha_s_sq + 1E-9) / (1 - alpha_t_sq + 1E-9) \
+                        * (1 - (alpha_t_sq + 1E-9) / (alpha_s_sq + 1E-9))
+            det_level = (1 - alpha_s_sq - sigma_t_2).sqrt()
+            stoch_level = torch.sqrt(
+                1 - (alpha_t_sq + 1E-9) / (alpha_s_sq + 1E-9)
+            )
         else:
-            stoch_level = torch.sqrt((1-alpha_s+1E-9) / (1-alpha_t+1E-9)) \
-                        * torch.sqrt(1-(alpha_t+1E-9) / (alpha_s+1E-9))
+            stoch_level = torch.sqrt(
+                (1 - alpha_s_sq + 1E-9) / (1 - alpha_t_sq + 1E-9)
+            ) * torch.sqrt(1 - (alpha_t_sq + 1E-9) / (alpha_s_sq + 1E-9))
             stoch_level = self.eta * stoch_level
-            det_level = (1-alpha_s-stoch_level.pow(2)).sqrt()
+            det_level = (1 - alpha_s_sq - stoch_level.pow(2)).sqrt()
         return det_level, stoch_level
 
     def forward(
@@ -64,11 +67,13 @@ class DDIMSampler(BaseSampler):
         prev_step = step-1/self.timesteps
         gamma_t = self.scheduler.get_gamma(step)
         gamma_s = self.scheduler.get_gamma(step-1/self.timesteps)
-        var_t = torch.sigmoid(gamma_t)
-        var_s = torch.sigmoid(gamma_s)
-        alpha_sqrt_s = (1-var_s).sqrt()
+        var_t = torch.sigmoid(-gamma_t)
+        var_s = torch.sigmoid(-gamma_s)
+        alpha_t = (1-var_t).sqrt()
+        alpha_s = (1-var_s).sqrt()
+        sigma_t = var_t.sqrt()
         noise_level = self._estimate_stoch_level(
-            alpha_t=1 - var_t, alpha_s=1 - var_s
+            alpha_t_sq=1 - var_t, alpha_s_sq=1 - var_s
         )
 
         # Estimate predictions
@@ -78,10 +83,10 @@ class DDIMSampler(BaseSampler):
         prediction = self.denoising_model(
             in_data, time_tensor, mask=mask, **conditioning
         )
-        state = (in_data-var_t.sqrt()*prediction) / (1-var_t).sqrt()
+        state = (in_data-sigma_t*prediction) / alpha_t
 
         if prev_step > 0:
             noise = torch.randn_like(in_data)
-            state = alpha_sqrt_s * state + noise_level[0] * prediction \
+            state = alpha_s * state + noise_level[0] * prediction \
                     + noise_level[1] * noise
         return state
--- ddm_dynamical/sampler/ddpm.py
+++ ddm_dynamical/sampler/ddpm.py
@@ -32,17 +32,21 @@ class DDPMSampler(BaseSampler):
         prev_step = step-1/self.timesteps
         gamma_t = self.scheduler.get_gamma(step)
         gamma_s = self.scheduler.get_gamma(prev_step)
-        var_t = torch.sigmoid(gamma_t)
-        alpha_t = (1-var_t)
-        alpha_s = torch.sigmoid(-gamma_s)
-        alpha_dash_t = alpha_t / alpha_s
-        alpha_sqrt_s = alpha_s.sqrt()
-        alpha_dash_sqrt_t = alpha_dash_t.sqrt()
+        var_t = torch.sigmoid(-gamma_t)
+        sigma_t = var_t.sqrt()
+        alpha_t_sq = 1-var_t
+        alpha_s_sq = torch.sigmoid(gamma_s)
+        alpha_dash_t_sq = alpha_t_sq / alpha_s_sq
+        alpha_t = alpha_t_sq.sqrt()
+        alpha_s = alpha_s_sq.sqrt()
+        alpha_dash_t = alpha_dash_t_sq.sqrt()
 
         # Estimate factors
-        latent_factor = alpha_dash_sqrt_t*(1-alpha_s)/(1-alpha_t)
-        state_factor = alpha_sqrt_s*(1-alpha_dash_t)/(1-alpha_t)
-        noise_factor = ((1-alpha_dash_t)*(1-alpha_s)/(1-alpha_t)).sqrt()
+        latent_factor = alpha_dash_t*(1-alpha_s_sq)/(1-alpha_t_sq)
+        state_factor = alpha_s*(1-alpha_dash_t_sq)/(1-alpha_t_sq)
+        noise_factor = (
+                (1-alpha_dash_t_sq)*(1-alpha_s_sq)/(1-alpha_t_sq)
+        ).sqrt()
 
         # Estimate tensors
         time_tensor = torch.ones(
@@ -51,7 +55,7 @@ class DDPMSampler(BaseSampler):
         prediction = self.denoising_model(
             in_data, time_tensor, mask=mask, **conditioning
         )
-        state = (in_data-var_t.sqrt()*prediction) / (1-var_t).sqrt()
+        state = (in_data-sigma_t*prediction) / alpha_t
 
         if prev_step > 0:
             noise = torch.randn_like(in_data)
