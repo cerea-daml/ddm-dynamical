ee80efe7cfffa5f816cf4bc7ed440c286c98f9bc
tobifinn <tobias.finn@enpc.fr>
2023-10-20 14:43:00 +0200

    Use of normalized gamma in sampling functions

--- ddm_dynamical/sampler/ddpm.py
+++ ddm_dynamical/sampler/ddpm.py
@@ -55,7 +55,7 @@ class DDPMSampler(BaseSampler):
             in_data.size(0), 1, device=in_data.device, dtype=in_data.dtype
         ) * step
         prediction = self.network(
-            in_data, norm_gamma_t, mask=mask, **conditioning
+            in_data, normalized_gama=norm_gamma_t, mask=mask, **conditioning
         )
         state = self.proj_func(
             prediction=prediction,
