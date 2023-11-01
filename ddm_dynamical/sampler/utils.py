c182e52b19f6319da32af9ea5af41dad8300ae4d
tobifinn <tobias.finn@enpc.fr>
2023-11-01 10:43:28 +0100

    Added default function to concatenate conditioning information with the in data

--- ddm_dynamical/sampler/utils.py
+++ ddm_dynamical/sampler/utils.py
@@ -19,6 +19,24 @@ import torch
 main_logger = logging.getLogger(__name__)
 
 
+def cat_conditioning(
+        in_data: torch.Tensor,
+        alpha: torch.Tensor,
+        sigma: torch.Tensor,
+        gamma: torch.Tensor,
+        mask: torch.Tensor = None,
+        **conditioning
+) -> torch.Tensor:
+    """
+    The default preprocessing function for the sampler. If conditioning data is
+    given, `in_data` will be concatenated with the conditioning data.
+    """
+    if conditioning:
+        return torch.cat((in_data, *conditioning.values()), dim=1)
+    else:
+        return in_data
+
+
 def project_to_state(
         prediction: torch.Tensor,
         in_data: torch.Tensor,
@@ -28,4 +46,9 @@ def project_to_state(
         mask: torch.Tensor = None,
         **conditioning
 ) -> torch.Tensor:
+    """
+    A default function projecting  `in_data` with the prediction to the
+    (cleaned) state. This function applies Tweedie's formula, Efron (2011), for
+    the projection.
+    """
     return (in_data-sigma*prediction) / alpha
