ae884976a5202741cc2feb9535bd77baa2410033
tobifinn <tobias.finn@enpc.fr>
2023-11-08 12:28:34 +0100

    Fixed bug with pbar for k diffusion

--- ddm_dynamical/sampler/k_diffusion.py
+++ ddm_dynamical/sampler/k_diffusion.py
@@ -100,5 +100,5 @@ class KDiffusionSampler(BaseSampler):
             in_tensor_exploded,
             sigma_tildes,
             extra_args=dict(mask=mask, **conditioning),
-            disable=~self.pbar
+            disable=None if self.pbar else True
         )
