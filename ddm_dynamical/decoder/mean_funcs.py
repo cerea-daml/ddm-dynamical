d2fd278f60cd74e14fd53179c5adfbb491454b3a
tobifinn <tobias.finn@enpc.fr>
2023-12-15 15:46:31 +0100

    Added mean funcs

---
+++ ddm_dynamical/decoder/mean_funcs.py
@@ -0,0 +1,36 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Created on 15/12/2023
+# Created for ddm_dynamical
+#
+# @author: Tobias Sebastian Finn, tobias.finn@enpc.fr
+#
+#    Copyright (C) {2023}  {Tobias Sebastian Finn}
+
+# System modules
+import logging
+
+# External modules
+import torch
+
+# Internal modules
+
+main_logger = logging.getLogger(__name__)
+
+
+def state_mean(
+        self,
+        in_tensor: torch.Tensor,
+        first_guess: torch.Tensor
+):
+    return in_tensor * self.std + self.mean
+
+
+def delta_mean(
+        self,
+        in_tensor: torch.Tensor,
+        first_guess: torch.Tensor
+):
+    dynamics = in_tensor * self.std + self.mean
+    return first_guess + dynamics
