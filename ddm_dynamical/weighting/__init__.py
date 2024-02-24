8b9e58311900393ce1966225603f1f694bb2a590
tobifinn <tobias.finn@enpc.fr>
2024-02-24 10:26:01 +0100

    Added exponential weighting to init

--- ddm_dynamical/weighting/__init__.py
+++ ddm_dynamical/weighting/__init__.py
@@ -1,10 +1,12 @@
 from .edm import *
 from .elbo import *
+from .exp import *
 from .sigmoid import *
 
 
 __all__ = [
     "EDMWeighting",
     "ELBOWeighting",
+    "ExponentialWeighting",
     "SigmoidWeighting"
 ]
