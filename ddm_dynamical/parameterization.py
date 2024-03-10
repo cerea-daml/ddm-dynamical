a829ae9f6d8244827cc82233e74129f3a86ff479
tobifinn <tobias.finn@enpc.fr>
2024-03-10 16:10:54 +0100

    Added parameterization for flow matching with optimal transport

--- ddm_dynamical/parameterization.py
+++ ddm_dynamical/parameterization.py
@@ -105,3 +105,84 @@ class DataParam(torch.nn.Module):
             *args, **kwargs
     ):
         return prediction
+
+
+class FlowOTParam(torch.nn.Module):
+    def estimate_errors(
+            self,
+            prediction: torch.Tensor,
+            in_data: torch.Tensor,
+            target: torch.Tensor,
+            noise: torch.Tensor,
+            alpha: torch.Tensor,
+            sigma: torch.Tensor,
+            gamma: torch.Tensor,
+            *args, **kwargs
+    ) -> torch.Tensor:
+        # Eq. 77, Kingma and Gao, 2023
+        o_target = target - noise
+        o_weight = (1 + torch.exp(-gamma * 0.5)).pow(-2)
+        return o_weight * (o_target - prediction).pow(2)
+
+    def forward(
+            self,
+            prediction: torch.Tensor,
+            in_data: torch.Tensor,
+            alpha: torch.Tensor,
+            sigma: torch.Tensor,
+            gamma: torch.Tensor,
+            *args, **kwargs
+    ):
+        # Assuming FlowOTScheduler for sampling
+        # Invert Eq. 63 Kingma and Gao, 2023
+        time = torch.sigmoid(-gamma/2)
+        return in_data - prediction * time
+
+
+class FParam(torch.nn.Module):
+    def __init__(self, sigma_data: float = 1.):
+        super().__init__()
+        self.sigma_data = sigma_data
+
+    def estimate_errors(
+            self,
+            prediction: torch.Tensor,
+            in_data: torch.Tensor,
+            target: torch.Tensor,
+            noise: torch.Tensor,
+            alpha: torch.Tensor,
+            sigma: torch.Tensor,
+            gamma: torch.Tensor,
+            *args, **kwargs
+    ) -> torch.Tensor:
+        # Eq. 119 in Kingma & Gao 2023
+        target_factor = (
+            torch.sqrt(torch.exp(-gamma) + self.sigma_data**2)
+        )/(
+            torch.exp(-0.5 * gamma) * self.sigma_data
+        )
+        in_data_factor = (
+            self.sigma_data * alpha
+        ) / (
+            torch.exp(-0.5 * gamma)
+            * torch.sqrt(torch.exp(-gamma) + self.sigma_data**2)
+        )
+        f_target = target_factor * target - in_data_factor * in_data
+        # Eq. 125
+        f_weighting = torch.exp(-gamma) / self.sigma_data**2 + 1
+        return (f_target-prediction).pow(2)/f_weighting
+
+    def forward(
+            self,
+            prediction: torch.Tensor,
+            in_data: torch.Tensor,
+            alpha: torch.Tensor,
+            sigma: torch.Tensor,
+            gamma: torch.Tensor,
+            *args, **kwargs
+    ):
+        # Eq. 118
+        sigma_tilde = torch.exp(-0.5 * gamma)
+        denom = sigma_tilde**2 + self.sigma_data ** 2
+        in_data_factor = (self.sigma_data**2 * alpha) / denom
+        f_factor = sigma_tilde * self.sigma_data / denom.sqrt()
