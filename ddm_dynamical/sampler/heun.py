fafe4e86b302a0b07869e938bee9e5a00ba28c91
tobifinn <tobias.finn@enpc.fr>
2024-03-25 16:10:12 +0100

    Fixed bugs in heun sampler

--- ddm_dynamical/sampler/heun.py
+++ ddm_dynamical/sampler/heun.py
@@ -65,8 +65,11 @@ class HeunSampler(BaseSampler):
         sigma_s = var_s.sqrt()
 
         # Coefficients for exploding
-        sigma_tilde_t = 1 / torch.exp(-0.5 * gamma_t)
-        sigma_tilde_s = 1 / torch.exp(-0.5 * gamma_s)
+        to_preserved_t = (1 + 1 / gamma_t.exp()).sqrt()
+        to_perserved_s = (1 + 1 / gamma_s.exp()).sqrt()
+
+        sigma_tilde_t = gamma_t.exp() ** (-0.5)
+        sigma_tilde_s = gamma_s.exp() ** (-0.5)
         dt = sigma_tilde_s - sigma_tilde_t
 
         # Estimate predictions
@@ -87,11 +90,11 @@ class HeunSampler(BaseSampler):
         )
 
         # Estimate grad in exploding
-        in_exploded = in_data / alpha_t
+        in_exploded = in_data / to_preserved_t
         grad = (in_exploded - denoised) / sigma_tilde_t
 
         out_exploded = in_exploded + grad * dt
-        out_preserved = out_exploded * alpha_t
+        out_preserved = out_exploded * to_perserved_s
         if prev_step > 0 and self.heun:
             # Heun step
             prediction = self.estimate_prediction(
@@ -112,5 +115,5 @@ class HeunSampler(BaseSampler):
             grad_2 = (out_exploded - denoised) / sigma_tilde_s
             total_grad = (grad + grad_2) / 2
             out_exploded = in_exploded + total_grad * dt
-            out_preserved = out_exploded * alpha_t
+            out_preserved = out_exploded * to_perserved_s
         return out_preserved
